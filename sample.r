4638a3d3d44cd466ce78abe067e517620040ba64
noumaan <noumaanahamed2@gmail.com>
2023-06-21 14:25:30 +0530

    sorted array

--- sample.r
+++ sample.r
@@ -1,8 +1,14 @@
-oddEven = function(x){
-  if(x%%2 == 0){
-    print("number is even")
-  }else{
-    print("number is odd")
-  }
-}
-oddEven(4)
+# oddEven = function(x){
+#   if(x%%2 == 0){
+#     print("number is even")
+#   }else{
+#     print("number is odd")
+#   }
+# }
+# oddEven(4)
+
+x <- c(2,3,1,67,89,4,6)
+
+y <- sort(x)
+
+cat("Sorted array:",y,"\n");
