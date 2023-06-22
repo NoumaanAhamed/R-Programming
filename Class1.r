6b1be4ae35d068bb3fcfaa6e001e742ed7041983
noumaan <noumaanahamed2@gmail.com>
2023-06-22 10:02:36 +0530

    added more

--- Class1.r
+++ Class1.r
@@ -8,3 +8,18 @@ m2<-c(80,81,82,83,84)
 m3<-c(95,96,97,98,99)
 d<-data.frame(s.no,regno,name,m1,m2,m3)
 print(d)
+
+rb<-rbind(s.no,regno,name,m1,m2,m3)
+rb
+cb<-cbind(s.no,regno,name,m1,m2,m3)
+cb
+seq(1,4,by=0.5)
+plot(d)
+head(d)
+tail(d)
+summary(d)
+str(d)
+d$s.no
+d[3,3]
+mat = matrix(seq(1:3),3,3)
+mat
